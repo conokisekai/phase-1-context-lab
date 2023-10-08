f48b52121cfaca44e0dbf92e2a876118dc7bf6dc
Conrad Kambi <kambiconrad@gmail.com>
2023-10-08 11:13:27 +0300

    Clean up code by removing comments.

--- index.js
+++ index.js
@@ -1,66 +1,71 @@
-// Helper function to create an employee record
 function createEmployeeRecord(arr) {
-    return {
-      firstName: arr[0],
-      familyName: arr[1],
-      title: arr[2],
-      payPerHour: arr[3],
-      timeInEvents: [],
-      timeOutEvents: []
-    };
-  }
-
-  // Helper function to create employee records from an array of arrays
-function createEmployeeRecords(arrays) {
-    return arrays.map(createEmployeeRecord);
-  }
-
-  // Helper function to create timeIn event
-function createTimeInEvent(dateStamp) {
-    const [date, hour] = dateStamp.split(' ');
-    this.timeInEvents.push({ type: 'TimeIn', date: date, hour: parseInt(hour, 10) });
-    return this;
-  }
-
-  // Helper function to create timeOut event
-function createTimeOutEvent(dateStamp) {
-    const [date, hour] = dateStamp.split(' ');
-    this.timeOutEvents.push({ type: 'TimeOut', date: date, hour: parseInt(hour, 10) });
-    return this;
-  }
-
-  // Helper function to calculate hours worked on a specific date
-function hoursWorkedOnDate(date) {
-    const timeIn = this.timeInEvents.find(event => event.date === date).hour;
-    const timeOut = this.timeOutEvents.find(event => event.date === date).hour;
-    return (timeOut - timeIn) / 100;
-  }
-
-  // Helper function to calculate pay earned on a specific date
-function wagesEarnedOnDate(date) {
-    const hoursWorked = hoursWorkedOnDate.call(this, date);
-    return hoursWorked * this.payPerHour;
-  }
-
-  // Helper function to find an employee by first name in an array of employee records
-function findEmployeeByFirstName(srcArray, firstName) {
-    return srcArray.find(record => record.firstName === firstName);
-  }
-
-  // Helper function to calculate total pay for all employees for all dates worked
-function calculatePayroll(employeeRecords) {
-    return employeeRecords.reduce((total, record) => total + allWagesFor.call(record), 0);
-  }
-  
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+  return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
 }
 
+function createEmployeeRecords(arrays) {
+  return arrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(' ');
+  this.timeInEvents.push({
+    type: 'TimeIn',
+    date: date,
+    hour: parseInt(hour, 10),
+  });
+  return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(' ');
+  this.timeOutEvents.push({
+    type: 'TimeOut',
+    date: date,
+    hour: parseInt(hour, 10),
+  });
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find((event) => event.date === date).hour;
+  const timeOut = this.timeOutEvents.find((event) => event.date === date).hour;
+  return (timeOut - timeIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find((record) => record.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce(
+    (total, record) => total + allWagesFor.call(record),
+    0
+  );
+}
+
+const allWagesFor = function () {
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
+
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  );
+
+  return payable;
+};
